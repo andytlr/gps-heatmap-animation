1c245433f9e76c7d628d59fb24adb717ac3d6b24
Andy Taylor <andytlr@gmail.com>
2014-02-16 08:23:23 +1100

    Got something that works.

--- animated-heatmap/replace.rb
+++ animated-heatmap/replace.rb
@@ -1,17 +1,40 @@
 class String
   def replace_content
-    to_replace = scan("replace_me")
+    # to_replace = scan("replace_me")
     # gsub("replace_me", "wang")
+
+    # gsub("replace_me") {|num| num.to_i+1}
+
+    # count = (count || '0').to_i + 1
+    # gsub("replace_me", "#{count}")
 
     # to_replace.length.times do |n|
     #   "Derp"
     # end
+
+    # to_replace.each do |i|
+    #   #some stuff
+    # end
   end
 end
 
-filename = "process.txt"
-outdata = File.read(filename).replace_content
+# Will scan for [replace]
+# begin="[replace]"
+# begin="[replace]"
 
-File.open(filename, 'w') do |out|
-  out << outdata
+filename = "process.txt"
+
+File.open(filename, 'r') do |f|
+  i = 0
+  contents = f.read.gsub('begin="[replace]"') { |m|
+    m.sub('[replace]', (i += 0.25).to_s)
+  }
+  puts contents
 end
+
+# filename = "process.txt"
+# outdata = File.read(filename).replace_content
+
+# File.open(filename, 'w') do |out|
+#   out << outdata
+# end
