66a4e9bbb430c52facd4f2b45743450e0bfe61ee
Andy Taylor <andytlr@gmail.com>
2014-02-16 08:38:24 +1100

    Refined script slightly.

--- animated-heatmap/replace.rb
+++ animated-heatmap/replace.rb
@@ -1,14 +1,21 @@
-# Will scan for [replace]
+# Will scan for [replace] in the file and replace it with an incremented number.
+# Eg this:
 # begin="[replace]"
 # begin="[replace]"
+# Would become:
+# begin="1"
+# begin="2"
+# If incrementing by 1
 
 filename = "heatmap.svg"
 
 class String
   def replace_content
     i = 0
+    increment_by = 0.05
+
     gsub('begin="[replace]"') { |m|
-      m.sub('[replace]', (i += 0.25).to_s)
+      m.sub('[replace]', (i += increment_by).to_s)
     }
   end
 end
