a9b27d810dc37258d5372e834ce511106dcbb7be
Andy Taylor <andytlr@gmail.com>
2014-03-02 11:49:39 +1100

    Script processes LOL from Illustrator into usable paths.

--- polyline-to-path.rb
+++ polyline-to-path.rb
@@ -2,9 +2,9 @@ filename = "Rplots.svg"
 
 class String
   def replace_content
-    gsub('<polyline', '<path').gsub('points="', 'd="M')
+    # gsub('<polyline', '<path').gsub('points="', 'd="M')
     # The variation below will also strip out all the other shit not needed by the D3 animation.
-    # gsub('<polyline', '<path').gsub('points="', 'd="M').gsub('opacity="0.125" fill="none" stroke="#000000" stroke-width="0.75" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"', 'fill="none"')
+    gsub('<polyline', '<path').gsub('points="', 'd="M').gsub('opacity="0.125" fill="none" stroke="#000000" stroke-width="0.75" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"', 'fill="none"')
   end
 end
 
